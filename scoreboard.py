a47f3d64ac1d0b3d8db6b5bae63a8cb0ef98356c
Vijay Soundaram <vijay6206@gmail.com>
2024-02-05 14:10:42 -0700

    scoreboard.py
    creating score board for pingpong

--- scoreboard.py
+++ scoreboard.py
@@ -10,4 +10,21 @@ class Scoreboard(Turtle):
         self.r_score = 0
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 80))
+        # self.goto(-100, 200)
+        # self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        # self.goto(100, 200)
+        # self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+
+
+    def l_point(self):
+        self.l_score += 1
+
 
